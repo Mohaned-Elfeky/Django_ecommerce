75f340239f514f18182d80d3053e653fc2c54e42
Mohaned Elfeky <MohanedElfeky@outlook.com>
2021-07-12 01:29:09 +0200

    Update settings.py

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -155,5 +156,5 @@ AWS_DEFAULT_ACL= None
 
 # AWS_LOCATION="img/"
 
-import django_heroku
+
 django_heroku.settigns(locals(), staticfiles=False)
